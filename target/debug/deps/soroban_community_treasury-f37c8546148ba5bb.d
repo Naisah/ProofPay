5ae1aafb715649be9dba4d61a5c0de6c08c3fd59
Naisah <naisahaspiras24@gmail.com>
2026-03-30 18:50:14 +0800

    Add ProofPay smart contract implementation

--- target/debug/deps/soroban_community_treasury-f37c8546148ba5bb.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Naisah\ProofPay\target\debug\deps\soroban_community_treasury-f37c8546148ba5bb.d: src\lib.rs
-
-C:\Users\Naisah\ProofPay\target\debug\deps\soroban_community_treasury-f37c8546148ba5bb.exe: src\lib.rs
-
-src\lib.rs:
--- target/wasm32-unknown-unknown/release/soroban_community_treasury.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Naisah\ProofPay\target\wasm32-unknown-unknown\release\soroban_community_treasury.wasm: C:\Users\Naisah\ProofPay\src\lib.rs
